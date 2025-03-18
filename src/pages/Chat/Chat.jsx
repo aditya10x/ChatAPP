2f022f96173c29e2bb4111ece4398e0492dc6c0a
unknown <adityapatibandha2@gmail.com>
2025-03-18 09:59:27 +0530

    updated files

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import "./Chat.css"
 const Chat = () => {
   return (
-    <div>
-      we are on the chat page
+    <div className='chat'>
+      
     </div>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 import assets from '../../assets/assets'
 
 const Login = () => {
+
+    const[currState,setCurrState] = useState("Sign Up");
+
   return (
     <div className='Login'>
       
@@ -10,18 +13,25 @@ const Login = () => {
       <form className="login-form">
 
         <h2>
-          Sign Up
+          {currState}
         </h2>
-        <input type="text" placeholder="Username" className="form-input" required/>
-        <input type="email" placeholder='Email Address' className="form-input" />
-        <input type="password" placeholder='password' className="form-input" />
-        <button type='submit'>Sign Up</button>
+        {currState == 'Sign Up'?<input type="text" placeholder="Username" className="form-input" required/>:null}
+        <input type="email" placeholder='Email Address' className="form-input" required />
+        <input type="password" placeholder='password' className="form-input" required />
+        <button type='submit'>{currState == "Sign Up"? "Create Account":"Login Now"}</button>
         <div className='login-term'>
           <input type="checkbox"/>
           <p>Agree To Terms And Conditions</p>
         </div>
         <div className='login-forgot'>
-          <p className='login-toggle'>Already have an account <span>Click Here</span>  </p>
+          {currState == 'Sign Up'
+          ? 
+          <p className='login-toggle'>Already have an account <span onClick={()=>setCurrState("Login")}>Login Here</span></p>
+          :
+          <p className='login-toggle'>Create an account <span onClick={()=>setCurrState("Sign Up")}>Click Here</span></p>
+          }
+          
+          
         </div>
       </form>   
       
